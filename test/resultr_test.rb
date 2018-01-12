f053ce175b5f30ad9d8fbd0743aef3ea5b38d8b4
Leonardo Camelo <camelo@pluga.co>
2018-01-12 14:35:54 -0200

    main api test

--- test/resultr_test.rb
+++ test/resultr_test.rb
@@ -1,7 +1,27 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-describe 'Resultr' do
+describe Resultr do
   it 'has a version number' do
-    expect(Resultr::VERSION).wont_be_nil
+    refute_nil Resultr::VERSION
+  end
+
+  describe '.ok' do
+    it 'returns a success result with given value' do
+      result = Resultr.ok('foo')
+
+      assert result.ok?
+      assert_equal 'foo', result.value
+    end
+  end
+
+  describe '.err' do
+    it 'results a failed result with given reason' do
+      result = Resultr.err('bar')
+
+      assert result.err?
+      assert_equal 'bar', result.reason
+    end
   end
 end
