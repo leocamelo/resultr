b9c8246c67c8c802fab5e8fa2de14447e2860c4b
Leonardo Camelo <camelo@pluga.co>
2018-01-16 10:19:10 -0200

    organize tests

--- test/resultr/result_test.rb
+++ test/resultr/result_test.rb
@@ -23,6 +23,22 @@ describe Resultr::Result do
     end
   end
 
+  describe '#and_then' do
+    it 'runs a given block with result value when it has success' do
+      result = Resultr::Result.new(:ok, 1)
+      new_result = result.and_then { |value| Resultr.ok(value + 1) }
+
+      assert_equal 2, new_result.value
+    end
+
+    it 'dont runs a given block and return self when result has failed' do
+      result = Resultr::Result.new(:err, 1)
+      new_result = result.and_then { |value| Resultr.ok(value + 1) }
+
+      assert_equal result, new_result
+    end
+  end
+
   describe '#expect!' do
     it 'returns the given value when the result has success' do
       result = Resultr::Result.new(:ok, 'foo')
@@ -38,22 +54,6 @@ describe Resultr::Result do
       end
 
       assert_equal 'bar', error.message
-    end
-  end
-
-  describe '#and_then' do
-    it 'runs a given block with result value when it has success' do
-      result = Resultr::Result.new(:ok, 1)
-      new_result = result.and_then { |value| Resultr.ok(value + 1) }
-
-      assert_equal 2, new_result.value
-    end
-
-    it 'dont runs a given block and return self when result has failed' do
-      result = Resultr::Result.new(:err, 1)
-      new_result = result.and_then { |value| Resultr.ok(value + 1) }
-
-      assert_equal result, new_result
     end
   end
 
